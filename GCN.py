877addfafa74175922e346d2847cbd1b3e082b7a
ChenQiPKU <chenqixianzi@gmail.com>
2019-04-08 18:54:53 +0800

    GCN

---
+++ GCN.py
@@ -0,0 +1,65 @@
+import os.path as osp
+
+import torch
+import torch.nn.functional as F
+from torch_geometric.datasets import Planetoid
+import torch_geometric.transforms as T
+from torch_geometric.nn import GCNConv
+
+dataset = 'Cora'
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', dataset)
+dataset = Planetoid(path, dataset, T.TargetIndegree())
+data = dataset[0]
+
+data.train_mask = torch.zeros(data.num_nodes, dtype=torch.uint8)
+data.train_mask[:data.num_nodes - 1000] = 1
+data.val_mask = None
+data.test_mask = torch.zeros(data.num_nodes, dtype=torch.uint8)
+data.test_mask[data.num_nodes - 500:] = 1
+
+
+class GCN(torch.nn.Module): # 2-layer
+    def __init__(self):
+        super(GCN, self).__init__()
+        self.conv1 = GCNConv(dataset.num_features, 16, cached=True)
+        self.conv2 = GCNConv(16, dataset.num_classes, cached=True)
+
+    def forward(self):
+        x, edge_index = data.x, data.edge_index
+        x = F.relu(self.conv1(x, edge_index))
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model, data = Net().to(device), data.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+
+
+def train():
+    model.train()
+    optimizer.zero_grad()
+    F.nll_loss(model()[data.train_mask], data.y[data.train_mask]).backward()
+    optimizer.step()
+
+
+def test():
+    model.eval()
+    logits, accs = model(), []
+    for _, mask in data('train_mask', 'val_mask', 'test_mask'):
+        pred = logits[mask].max(1)[1]
+        acc = pred.eq(data.y[mask]).sum().item() / mask.sum().item()
+        accs.append(acc)
+    return accs
+
+
+best_val_acc = test_acc = 0
+for epoch in range(1, 201):
+    train()
+    train_acc, val_acc, tmp_test_acc = test()
+    if val_acc > best_val_acc:
+        best_val_acc = val_acc
+        test_acc = tmp_test_acc
+    log = 'Epoch: {:03d}, Train: {:.4f}, Val: {:.4f}, Test: {:.4f}'
+    print(log.format(epoch, train_acc, best_val_acc, test_acc))
